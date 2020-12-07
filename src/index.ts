b6163b09fccb7359c073d940a9d9233bfc32ebd5
Tom <tvandort1@gmail.com>
2020-12-06 20:09:09 -0500

    Runner.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, world.");
+import { program } from "commander";
+
+program.option(
+  "-d, --day <number>",
+  "Prints the solution for a given day.",
+  (number) => parseInt(number)
+);
+
+program.parse(process.argv);
+
+if (isNaN(program.day)) {
+  console.log("--day parameter must be a number.");
+}
+
+if (program.day !== undefined) {
+  require(`./day-${program.day}`);
+}
+
+if (program.args.length === 0) {
+  console.log(program.help());
+}
--- watcher.js
+++ watcher.js
@@ -4,12 +4,9 @@ const { spawn } = require("child_process");
 const watch = new TscWatchClient();
 
 watch.on("success", () => {
-  const tester = spawn("yarn", ["test"], {
+  spawn("yarn", ["test"], {
     cwd: process.cwd(),
     stdio: "inherit",
-  });
-  tester.on("exit", () => {
-    watch.kill();
   });
 });
 
